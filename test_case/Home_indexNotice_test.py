2e539e881b711a02cf2b5d323730731c5b7de2d7
leaning002 <leaning002@126.com>
2018-09-03 11:13:13 +0800

    api_test

--- test_case/Home_indexNotice_test.py
+++ test_case/Home_indexNotice_test.py
@@ -7,7 +7,7 @@ Url = "https://mysteelapi.steelphone.com/v4/indexNotice.htm?"
 
 def test_indexNotice_success():
 	''''''
-	result = requests.post(Url,params = {'userId':'129647','machineCode':'2ACCCCDC5FBDBE59ADD70F1C100FE4BB'}).json()   #李平平
+	result = requests.post(Url,params = {'userId':'566453','machineCode':'90526B160362A7A4FECA22411080F8CF'}).json()   #李平平
 
 	'''首页短信红点'''
 	if result['smsNoticeCount'] == '':
@@ -18,18 +18,10 @@ def test_indexNotice_success():
 		print("有红点")
 		
 	'''首页“我的”红点'''
-	for x in range(1,5):
-		a = 0
-		url = 'https://mysteelapi.steelphone.com/v4/comment/reply/replyToMe.htm?&userId=129647&machineCode=2ACCCCDC5FBDBE59ADD70F1C100FE4BB&page=' + str(x)
-		resultms = requests.post(url).json()   #李平平
-		print(resultms)
-		a += int(resultms['smsNotice'])
-	if a == 0:
-		assert resultms['myNotice'] == '0'
+	if result['myNotice'] == '0':
 		print("没有红点")
 	else:
-		assert resultms['myNotice'] == '1'
 		print("有红点")
-			
+		
 if __name__ == '__main__':
 	pytest.main(["-s", "Home_indexNotice_test.py"])
